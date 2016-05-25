cf833b9c0b597715a8424ecf249012e0956459bd
akaFTS <gustavo84171@hotmail.com>
2016-05-24 21:20:27 -0300

    Verificando dimensoes 9

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var mathmode = require("mathmode");
 var fs = require("fs");
 var express = require('express');
 var q = require("q");
+var iss = require("image-size-stream");
 
 //configs
 var imports = ['amsmath', 'amssymb'];
@@ -76,6 +77,7 @@ bot.on('inline_query', function(msg)
     var timestamp = Date.now()+".jpeg";
     var imagefill = fs.createWriteStream("images/"+timestamp);
     var imgstream = mathmode(msg.query, options);
+    var sizestream = iss.createSizeStream();
 
     //tratamento de erro na compilação do latex
     var isOk = true;
@@ -83,7 +85,12 @@ bot.on('inline_query', function(msg)
         isOk = false;
     });
 
-    var piper = imgstream.pipe(imagefill);
+    var width = 200;
+    sizestream.on("size", function(dim){
+        width = parseInt(100*(dim.width/dim.height));
+    });
+
+    var piper = imgstream.pipe(sizestream).pipe(imagefill);
 
     //ao terminar a criação
     piper.on("finish", function(){
